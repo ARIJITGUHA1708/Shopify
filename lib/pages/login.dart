63354666754906943092e4a2e914b3c528fe5d89
ARIJITGUHA1708 <arijitguha1708@gmail.com>
2024-08-24 18:52:21 +0530

    modify the login page and add functionality to hit dummy url

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:demo_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -9,6 +10,10 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final username = TextEditingController();
+  final password = TextEditingController();
+  String? usernameString;
+  String? passwordString;
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -76,18 +81,60 @@ class _LoginState extends State<Login> {
                         margin: const EdgeInsets.only(top: 30),
                         width: MediaQuery.of(context).size.width * 0.95,
                         child: TextField(
+                          controller: username,
                           decoration: InputDecoration(
                               prefixIcon: const Icon(Icons.search_outlined),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(7),
                               ),
-                              hintText: 'Phone Number',
+                              hintText: 'Email',
                               hintStyle: const TextStyle(color: Colors.black38),
                               contentPadding:
                                   const EdgeInsets.only(top: 10, left: 5)),
-                          keyboardType: const TextInputType.numberWithOptions(),
+                          //keyboardType: const TextInputType.numberWithOptions(),
                         ),
-                      )
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 30),
+                        width: MediaQuery.of(context).size.width * 0.95,
+                        child: TextField(
+                          controller: password,
+                          decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.search_outlined),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(color: Colors.black38),
+                              contentPadding:
+                                  const EdgeInsets.only(top: 10, left: 5)),
+                          //keyboardType: const TextInputType.numberWithOptions(),
+                        ),
+                      ),
+                      Container(
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              setState(() {
+                                usernameString = username.text;
+                                passwordString = password.text;
+                              });
+                              try {
+                                var response = await http.post(
+                                    Uri.parse(
+                                        'https://dummyjson.com/auth/login'),
+                                    body: {
+                                      "username": "$usernameString",
+                                      "password": "$passwordString"
+                                    });
+                                print(response.body);
+                              } catch (error) {
+                                print(error);
+                              }
+                            },
+                            child: const Text("Submit")),
+                      ),
+                      Text("Username:$usernameString"),
+                      Text("Password:$passwordString")
                     ],
                   ),
                 ))
