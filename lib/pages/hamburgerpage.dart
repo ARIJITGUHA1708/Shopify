9a200a13607a58bd8d341bd33ff222231715d884
ARIJITGUHA1708 <arijitguha1708@gmail.com>
2024-08-25 22:10:03 +0530

    add conditional hamburger menu for logedin user and normal user

--- lib/pages/hamburgerpage.dart
+++ lib/pages/hamburgerpage.dart
@@ -1,4 +1,6 @@
+import 'package:demo_app/providers/login_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Hamburgerpage extends StatelessWidget {
   const Hamburgerpage({super.key});
@@ -10,99 +12,158 @@ class Hamburgerpage extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 215, 219, 234),
       width: MediaQuery.of(context).size.width * 0.8,
       //surfaceTintColor: const Color.fromARGB(255, 0, 62, 249),
-      child: ListView(
-        padding: const EdgeInsets.only(top: 0),
-        children: const [
-          SizedBox(
-            height: 100,
-            child: DrawerHeader(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                  Color.fromARGB(255, 18, 141, 242),
-                  Color.fromARGB(255, 209, 229, 245)
-                ])),
-                child: Text(
-                  "Shopsify",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w800, color: Colors.white),
-                )),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("My Profile"),
-          ),
-          Divider(
-            height: 1,
-            thickness: 2,
-            color: Colors.black12,
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("SuperCoin Zone"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Shopsify Plus Zone"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("All Categories"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("More on Shopsify"),
-          ),
-          ListTile(
-            leading: Icon(Icons.language),
-            title: Text("Choose Language"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Offer Zone"),
-          ),
-          Divider(
-            height: 1,
-            thickness: 2,
-            color: Colors.black12,
-          ),
-          ListTile(
-            leading: Icon(Icons.sell),
-            title: Text("Sell on Shopsify"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("My Orders"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Coupons"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("My Cart"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("My Wishlist"),
-          ),
-          ListTile(
-            leading: Icon(Icons.notification_add),
-            title: Text("My Notification"),
-          ),
-          Divider(
-            height: 1,
-            thickness: 2,
-            color: Colors.black12,
-          ),
-          ListTile(
-            leading: Icon(Icons.help_center),
-            title: Text("Help Center"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Legal"),
-          )
-        ],
+      child: Consumer<LoginProvider>(
+        builder: (context, value, child) => value.loginStatus
+            ? ListView(
+                padding: const EdgeInsets.only(top: 0),
+                children: const [
+                  SizedBox(
+                    height: 100,
+                    child: DrawerHeader(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                          Color.fromARGB(255, 18, 141, 242),
+                          Color.fromARGB(255, 209, 229, 245)
+                        ])),
+                        child: Text(
+                          "Shopsify",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, color: Colors.white),
+                        )),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("My Profile"),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Colors.black12,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("SuperCoin Zone"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Shopsify Plus Zone"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("All Categories"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("More on Shopsify"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.language),
+                    title: Text("Choose Language"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Offer Zone"),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Colors.black12,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.sell),
+                    title: Text("Sell on Shopsify"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("My Orders"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Coupons"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("My Cart"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("My Wishlist"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.notification_add),
+                    title: Text("My Notification"),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Colors.black12,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.help_center),
+                    title: Text("Help Center"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Legal"),
+                  )
+                ],
+              )
+            : ListView(
+                padding: const EdgeInsets.only(top: 0),
+                children: const [
+                  SizedBox(
+                    height: 100,
+                    child: DrawerHeader(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                          Color.fromARGB(255, 18, 141, 242),
+                          Color.fromARGB(255, 209, 229, 245)
+                        ])),
+                        child: Text(
+                          "Shopsify",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, color: Colors.white),
+                        )),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("SuperCoin Zone"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Shopsify Plus Zone"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("All Categories"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("More on Shopsify"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.language),
+                    title: Text("Choose Language"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Offer Zone"),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 2,
+                    color: Colors.black12,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.help_center),
+                    title: Text("Help Center"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("Legal"),
+                  )
+                ],
+              ),
       ),
     );
   }
